d0c3db720850f63f69d4e5108dfb0adc5b0ba1ba
Alex Sandberg <alexsandberg7@gmail.com>
2020-02-04 14:12:15 -0700

    Add truth table generator (not complete) and T/F string generator

--- project1.py
+++ project1.py
@@ -1,10 +1,15 @@
+
+
 # Rules:
 # every guilty suspect is lying
 # every innocent suspect is telling the truth
 
 
-def build_truth_table(*args, **kwargs):
-    pass
+# suspects –– modify as needed
+suspects = ['Paul', 'Quinn', 'Ray', 'Steve', 'Ted']
+
+# number of suspects
+suspect_num = len(suspects)
 
 
 class Accusation:
@@ -67,3 +72,56 @@ steve_accusation.create_conjunction('Quinn', 'Ray')
 # Ted says, “At least one of Paul or Ray is guilty.”
 ted_accusation = Accusation('Ted')
 ted_accusation.create_disjunction('Paul', 'Ray')
+
+
+# functions for creating truth table
+
+# generates T/F strings of given length
+def create_TF_string(total, repeat):
+    pattern = []
+
+    # iterations = num of times pattern repeats
+    iterations = int(total / 2 ** (repeat + 1))
+
+    for row in range(iterations):
+
+        # add T's
+        for index in range(2**repeat):
+            pattern.append('T')
+
+        # add F's
+        for index in range(2**repeat):
+            pattern.append('F')
+
+    return pattern
+
+
+def create_truth_table(suspect_num, suspects):
+    # number of rows needed = 2^suspect_num, +1 for header
+    rows = (2 ** suspect_num) + 1
+
+    # of columns needed is twice the number of suspects
+    # each suspect has a "x is guilty" proposition and their statement
+    columns = suspect_num * 2
+
+    truth_table = []
+
+    for index in range(columns):
+        # first half of columns
+        if index < suspect_num:
+            # get name and first initial
+            suspect = suspects[index]
+            suspect_initial = suspect[0].lower()
+
+            # get T/F pattern
+            pattern = create_TF_string((rows - 1), index)
+
+            # add suspect inital to pattern as header
+            pattern = [suspect_initial] + pattern
+
+            truth_table.append(pattern)
+
+    return truth_table
+
+
+print(create_truth_table(suspect_num, suspects))
