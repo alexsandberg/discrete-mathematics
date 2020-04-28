dd3b0fc498ab7a0b6ec0eba614d33dba47e45a2a
Alex Sandberg <alexsandberg7@gmail.com>
2020-04-28 15:27:04 -0600

    Update comments. Add comma separation to numbers on output

--- restaurant_probability/restaurant_probability.py
+++ restaurant_probability/restaurant_probability.py
@@ -2,6 +2,11 @@ import random
 import sys
 from decimal import Decimal
 
+# Author: Alex Sandberg-Bernard
+# Johns Hopkins University
+# Discrete Mathematics – EN.605.203.81.SP20
+
+
 # VALIDATION –––––––––––––––––––
 
 # ensure n arg is given
@@ -25,16 +30,32 @@ if n < 1:
 
 
 def trial():
+    '''
+    Runs a trial, which is accomplished by generating and returning a list of
+    randomly generated integers between 1 and 6.
+    '''
     result = [random.randint(1, 6) for a in range(12)]
-    # print(result)
+
     return result
 
 
 def check_trial(trial):
+    '''
+    Checks if a trial was a success by reducing the trial list to a set in order
+    to ignore duplicates, then comparing that set to the set of integers between
+    1 and 6. Returns a boolean value based on comparison.
+    '''
     return {1, 2, 3, 4, 5, 6} == set(trial)
 
 
 def run_trials(n, set):
+    '''
+    Executes a given number of trials and keeps track of the number of successes 
+    in the set. The first parameter is an integer value that determines the number
+    of trials. The second parameter is the set number, which is used to identify
+    the set. Trial data is written to an output file after each trial is run. An
+    integer value of number of successes in the trial is returned.
+    '''
     success = 0
 
     # write results to output.txt file
@@ -43,35 +64,57 @@ def run_trials(n, set):
         output_txt.write(f'\n-----------------------------------------\n')
         output_txt.write(f'\nSet {set+1}\n\n')
 
+        # run n trials
         for i in range(n):
+            # run the trial and check the result
             result = trial()
             check = check_trial(result)
             outcome = 'success' if check else 'failure'
 
+            # write result to output file
             output_txt.write(f'\n{result}\n')
             output_txt.write(f'{outcome}\n\n')
+
+            # if trial was success, incrememt successes
             if (check):
                 success += 1
+
     return success
 
 
 def series(n):
+    '''
+    Runs a series (10) of trials. Accepts integer value as parameter,
+    which is passed to run_trials method as argument for number of trials
+    to be run. Returns array of results, where each entry is an object
+    containing the number of successes in the trial and the success
+    probability of the trial.
+    '''
     results = []
     # run series of 10 sets of n trials
     for i in range(10):
+        # run trials and add successes to results
         successes = run_trials(n, i)
         results.append({
             'successes': successes,
             'probability': Decimal(successes) / Decimal(n)
         })
+
     return results
 
 
 def series_probability(results):
+    '''
+    Calculates and returns series probability estimate by averaging the
+    probability for each set of trials.
+    '''
     subtotal = 0
+
+    # iterate through each result and sum probabilities
     for entry in results:
         subtotal += entry['probability']
 
+    # return average probability
     return Decimal(subtotal) / Decimal(len(results))
 
 
@@ -80,13 +123,13 @@ results = series(n)
 probability = series_probability(results)
 
 
-print(f'\nTrials: {n}')
+# print results of trials to console
+print(f'\nTrials: {n:,}')
 print(f'\nSeries:')
 
-# print out results of each set of trials
 for num, result in enumerate(results):
     print(f'\n{num+1}.')
-    print(f"successes: {result['successes']}")
+    print(f"successes: {result['successes']:,}")
     print(f"set probability: {result['probability']}")
 
 print(f'\nseries success probability (average): {probability}\n')
@@ -94,11 +137,12 @@ print(f'\nseries success probability (average): {probability}\n')
 
 # write results to output.txt file
 with open('restaurant_probability_output.txt', mode='w') as output_txt:
-    output_txt.write(f'\nTrials: {n}\n\nSeries:')
-    # print out results of each set of trials
+    output_txt.write(f'\nTrials: {n:,}\n\nSeries:')
+
+    # write results of each set of trials
     for num, result in enumerate(results):
         output_txt.write(f'\n\n{num+1}.')
-        output_txt.write(f"successes: {result['successes']}\n")
+        output_txt.write(f"successes: {result['successes']:,}\n")
         output_txt.write(f"set probability: {result['probability']}")
     output_txt.write(
         f'\n\nseries success probability (average): {probability}\n')
